c27599c9b28e4631d15761f6c6614f84fe675cb8
a.kazantsev <a.kazantsev@tinkoff.ru>
2021-08-25 15:47:33 +0300

    fixes after CR

--- app/src/test/java/com/tinkoff/edu/AppTest.java
+++ app/src/test/java/com/tinkoff/edu/AppTest.java
@@ -9,6 +9,7 @@ import com.tinkoff.edu.app.StaticVariableLoanCalcRepo;
 import com.tinkoff.edu.app.interfaces.LoanCalcRepo;
 import com.tinkoff.edu.app.interfaces.LoanCalcService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,22 +20,24 @@ public class AppTest {
     private LoanRequest loanRequest;
 
     @BeforeEach
-    public void Init(){
+    public void Init() {
         loanRequest = new LoanRequest(ClientType.PERSON, 10, 1000);
         loanCalcRepo = new StaticVariableLoanCalcRepo();
         loanCalcService = new DefaultLoanCalcService(loanCalcRepo);
     }
 
     @Test
+    @DisplayName("Первый запрос должен иметь ID = 1")
     public void shouldGetId1WhenFirstCall() {
         LoanResponse loanResponse = new LoanCalcController(loanCalcService).createRequest(loanRequest);
-        assertEquals(1,loanResponse.getRequestId());
+        assertEquals(1, loanResponse.getRequestId(), "Expected 1 but actual " + loanResponse.getRequestId());
     }
 
     @Test
-    public void shouldGetIncrementedIdWhenAnyCall(){
+    @DisplayName("Номер запроса должен совпадать с размером списка")
+    public void shouldGetIncrementedIdWhenAnyCall() {
         LoanResponse loanResponse = new LoanCalcController(loanCalcService).createRequest(loanRequest);
-        assertEquals(loanCalcRepo.getRepoSize(),loanResponse.getRequestId());
+        assertEquals(loanCalcRepo.getRepoSize(), loanResponse.getRequestId(), "Expected" + loanCalcRepo.getRepoSize() + " but actual " + loanResponse.getRequestId());
     }
 
 }
